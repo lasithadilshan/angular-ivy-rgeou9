5ecad1e3471b6c0fd1bc3050b927b11605feccfc
lasithadilshan <dilshantilakaratne29@gmail.com>
2021-08-22 16:49:38 +0530

    5 files modified, 3 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  locations: string[] = ['Downtown', 'S. County', 'Lakeside'];
+
+  constructor(){ }
+
+  ngOnInit() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { RfsComponent } from './rfs/rfs.component';
+import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
-  declarations: [AppComponent, RfsComponent],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule {}
+export class AppModule { }
