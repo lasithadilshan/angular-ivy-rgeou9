793d7f8f5437cf1a7c695d4cee8cc7399e4f34c7
lasithadilshan <dilshantilakaratne29@gmail.com>
2021-08-22 18:16:53 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -7,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   locations: string[] = ['Downtown', 'S. County', 'Lakeside'];
+  volunteerForm: FormGroup
 
-  constructor(){ }
+  constructor(private fb: FormBuilder){ }
 
   ngOnInit() {
+    this.initializeForm();
+  }
 
+  initializeForm() : void {
+    this.volunteerForm = this.fb.group({
+      name: 'Name here',
+      phoneNumber: '',
+      preferredLocation: '',
+      animals: this.fb.group({
+        dogs: false,
+        cats: false,
+        reptiles: false
+      }),
+      references: this.fb.array([this.fb.control('')])
+    });
+  }
+  onSubmit(): void {
+    console.log(this.volunteerForm);
+  }
+
+  selectLocation(event): void {
+    this.volunteerForm.patchValue({
+      preferredLocation: event.target.value
+    });
+  }
+
+  addEmail(): void {
+    this.references.push(this.fb.control(''));
+  }
+
+  removeEmail(index: number): void{
+    this.references.removeAt(index);
+  }
+
+  get references(): FormArray{
+    return this.volunteerForm.get('references') as FormArray;
   }
 }
