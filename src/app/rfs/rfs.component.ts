1e6d2d3cda79e9cd633f70eef596b6441f7608a9
lasithadilshan <dilshantilakaratne29@gmail.com>
2021-08-22 16:02:50 +0530

    Initial commit

---
+++ src/app/rfs/rfs.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-rfs',
+  templateUrl: './rfs.component.html',
+  styleUrls: ['./rfs.component.css']
+})
+export class RfsComponent {
+  EmployeeName = new FormControl('');
+}
