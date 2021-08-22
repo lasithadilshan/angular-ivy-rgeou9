3df166cf4404ff66655ab46328baeebe55bbb001
lasithadilshan <dilshantilakaratne29@gmail.com>
2021-08-22 16:24:39 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { RfsComponent } from './rfs/rfs.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, RfsComponent],
-  declarations: [AppComponent, HelloComponent, RfsComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  declarations: [AppComponent, RfsComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
